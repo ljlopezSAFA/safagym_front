228b2117a21040eae9764021e4834359c0085a6b
rubenmt <rmatiastiscar@safareyes.es>
2021-11-25 20:32:47 +0100

    metemos crear ventana en un metodo

--- aplicacionPeliculas.py
+++ aplicacionPeliculas.py
@@ -6,24 +6,6 @@ from PIL import ImageTk, Image
 from tkinter import messagebox
 
 
-miRaiz = Tk()
-miRaiz.title("TOP PELICULAS IMDB")
-miRaiz.iconbitmap("3d_movies_folder_20527.ico")
-miFrame = Frame(miRaiz)
-miFrame.pack()
-miRaiz.resizable(width=False, height=False)
-
-imagenfondo = ImageTk.PhotoImage(Image.open("fondoAplicacion.gif").resize((500,400)))
-labelfondo = Label(miFrame,image=imagenfondo)
-labelfondo.grid(row=0,column=0,rowspan = 5,columnspan= 4)
-
-
-
-
-posicionvar= StringVar()
-titulovar= StringVar()
-aniovar= StringVar()
-valvar= StringVar()
 
 
 def peli_por_posicion():
@@ -123,77 +105,106 @@ def menu_mostrar():
 
 
 
-#-----------------------------------MENÚ--------------------------------
+def crear_ventana():
 
-barraMenu= Menu(miRaiz)
-miRaiz.config(menu=barraMenu)
+    miRaiz = Tk()
 
-archivoMenu= Menu(barraMenu,tearoff= 0)
-barraMenu.add_cascade(label="Archivo", menu=archivoMenu)
+    global posicionvar
+    global titulovar
+    global aniovar
+    global valvar
 
-archivoMenu.add_command(label="Mostrar Películas", command= menu_mostrar)
-archivoMenu.add_separator()
-archivoMenu.add_command(label= "Borrar Películas")
+    posicionvar = StringVar()
+    titulovar = StringVar()
+    aniovar = StringVar()
+    valvar = StringVar()
 
+    miRaiz.title("TOP PELICULAS IMDB")
+    miRaiz.iconbitmap("3d_movies_folder_20527.ico")
+    miFrame = Frame(miRaiz)
+    miFrame.pack()
+    miRaiz.resizable(width=False, height=False)
 
-
-
-
-#--------------------------LABELS-----------------------------------------
-
-labelPosicion = Label(miFrame,text="POSICIÓN :")
-labelPosicion.grid(row=0, column=0)
-labelPosicion.config(pady=20,height=0,bg="black",fg="orange",font=("Courier", 12, "bold"))
-
-labelTitulo = Label(miFrame,text="TÍTULO :")
-labelTitulo.grid(row=1, column=0)
-labelTitulo.config(pady=20,bg="black",fg="orange",font=("Courier", 12, "bold"))
-
-labelAnio = Label(miFrame,text="AÑO :")
-labelAnio.grid(row=2, column=0)
-labelAnio.config(pady=20,bg="black",fg="orange",font=("Courier", 12, "bold"))
-
-labelVal = Label(miFrame,text="VALORACIÓN:")
-labelVal.grid(row=3, column=0)
-labelVal.config(pady=20,bg="black",fg="orange",font=("Courier", 12, "bold"))
+    imagenfondo = ImageTk.PhotoImage(Image.open("fondoAplicacion.gif").resize((500,400)))
+    labelfondo = Label(miFrame,image=imagenfondo)
+    labelfondo.grid(row=0,column=0,rowspan = 5,columnspan= 4)
 
 
-#-----------------------------ENTRYS-----------------------------------
-
-entryPosicion= Entry(miFrame,textvariable= posicionvar)
-entryPosicion.grid(row=0, column=1, columnspan=2)
 
 
-entryTitulo= Entry(miFrame,textvariable= titulovar)
-entryTitulo.grid(row=1, column=1, columnspan=2)
+    #-----------------------------------MENÚ--------------------------------
 
-entryAnio= Entry(miFrame, textvariable= aniovar)
-entryAnio.grid(row=2, column=1, columnspan=2)
+    barraMenu= Menu(miRaiz)
+    miRaiz.config(menu=barraMenu)
 
-entryVal= Entry(miFrame, textvariable= valvar)
-entryVal.grid(row=3, column=1, columnspan=2)
+    archivoMenu= Menu(barraMenu,tearoff= 0)
+    barraMenu.add_cascade(label="Archivo", menu=archivoMenu)
 
-
-#------------------------------BOTONES------------------------------------------
-
-
-botonPosicion= Button(miFrame,text="OK", width=5, command= peli_por_posicion)
-botonPosicion.grid(row=0, column= 3,padx=10)
-
-botonTitulo= Button(miFrame,text="OK", width=5, command=peli_por_titulo)
-botonTitulo.grid(row=1, column= 3,padx=10)
+    archivoMenu.add_command(label="Mostrar Películas", command= menu_mostrar)
+    archivoMenu.add_separator()
+    archivoMenu.add_command(label= "Borrar Películas")
 
 
-botonAnio= Button(miFrame,text="OK", width=5, command= peli_por_anio)
-botonAnio.grid(row=2, column= 3,padx=10)
 
 
-botonVal= Button(miFrame,text="OK", width=5)
-botonVal.grid(row=3, column= 3,padx=10)
 
-botonInsertar= Button(miFrame, text="Insertar Pelicula",command= insertar_pelicula)
-botonInsertar.grid(row=4, column=1,columnspan=2,padx=10,pady=10)
+    #--------------------------LABELS-----------------------------------------
 
-miRaiz.mainloop()
+    labelPosicion = Label(miFrame,text="POSICIÓN :")
+    labelPosicion.grid(row=0, column=0)
+    labelPosicion.config(pady=20,height=0,bg="black",fg="orange",font=("Courier", 12, "bold"))
+
+    labelTitulo = Label(miFrame,text="TÍTULO :")
+    labelTitulo.grid(row=1, column=0)
+    labelTitulo.config(pady=20,bg="black",fg="orange",font=("Courier", 12, "bold"))
+
+    labelAnio = Label(miFrame,text="AÑO :")
+    labelAnio.grid(row=2, column=0)
+    labelAnio.config(pady=20,bg="black",fg="orange",font=("Courier", 12, "bold"))
+
+    labelVal = Label(miFrame,text="VALORACIÓN:")
+    labelVal.grid(row=3, column=0)
+    labelVal.config(pady=20,bg="black",fg="orange",font=("Courier", 12, "bold"))
 
 
+    #-----------------------------ENTRYS-----------------------------------
+
+    entryPosicion= Entry(miFrame,textvariable= posicionvar)
+    entryPosicion.grid(row=0, column=1, columnspan=2)
+
+
+    entryTitulo= Entry(miFrame,textvariable= titulovar)
+    entryTitulo.grid(row=1, column=1, columnspan=2)
+
+    entryAnio= Entry(miFrame, textvariable= aniovar)
+    entryAnio.grid(row=2, column=1, columnspan=2)
+
+    entryVal= Entry(miFrame, textvariable= valvar)
+    entryVal.grid(row=3, column=1, columnspan=2)
+
+
+    #------------------------------BOTONES------------------------------------------
+
+
+    botonPosicion= Button(miFrame,text="OK", width=5, command= peli_por_posicion)
+    botonPosicion.grid(row=0, column= 3,padx=10)
+
+    botonTitulo= Button(miFrame,text="OK", width=5, command=peli_por_titulo)
+    botonTitulo.grid(row=1, column= 3,padx=10)
+
+
+    botonAnio= Button(miFrame,text="OK", width=5, command= peli_por_anio)
+    botonAnio.grid(row=2, column= 3,padx=10)
+
+
+    botonVal= Button(miFrame,text="OK", width=5)
+    botonVal.grid(row=3, column= 3,padx=10)
+
+    botonInsertar= Button(miFrame, text="Insertar Pelicula",command= insertar_pelicula)
+    botonInsertar.grid(row=4, column=1,columnspan=2,padx=10,pady=10)
+
+    miRaiz.mainloop()
+
+
+
+crear_ventana()
